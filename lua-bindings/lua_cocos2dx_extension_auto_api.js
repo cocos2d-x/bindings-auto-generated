4a15f94c11edc3c7ffe98ca49913eca78c75eb47
CocosRobot <redmine@cocos2d-x.org>
2013-11-01 10:31:19 +0000

    [AUTO] : autogenerated bindings

--- lua-bindings/lua_cocos2dx_extension_auto_api.js
+++ lua-bindings/lua_cocos2dx_extension_auto_api.js
@@ -2051,6 +2051,101 @@ EditBox : function () {},
 };
 
 /**
+ * @class AssetsManager
+ */
+cc.AssetsManager = {
+
+/**
+ * @method setStoragePath
+ * @param {const char*}
+ */
+setStoragePath : function () {},
+
+/**
+ * @method setPackageUrl
+ * @param {const char*}
+ */
+setPackageUrl : function () {},
+
+/**
+ * @method checkUpdate
+ * @return A value converted from C/C++ "bool"
+ */
+checkUpdate : function () {},
+
+/**
+ * @method getStoragePath
+ * @return A value converted from C/C++ "const char*"
+ */
+getStoragePath : function () {},
+
+/**
+ * @method update
+ */
+update : function () {},
+
+/**
+ * @method setConnectionTimeout
+ * @param {unsigned int}
+ */
+setConnectionTimeout : function () {},
+
+/**
+ * @method setVersionFileUrl
+ * @param {const char*}
+ */
+setVersionFileUrl : function () {},
+
+/**
+ * @method getPackageUrl
+ * @return A value converted from C/C++ "const char*"
+ */
+getPackageUrl : function () {},
+
+/**
+ * @method getConnectionTimeout
+ * @return A value converted from C/C++ "unsigned int"
+ */
+getConnectionTimeout : function () {},
+
+/**
+ * @method getVersion
+ * @return A value converted from C/C++ "std::string"
+ */
+getVersion : function () {},
+
+/**
+ * @method getVersionFileUrl
+ * @return A value converted from C/C++ "const char*"
+ */
+getVersionFileUrl : function () {},
+
+/**
+ * @method deleteVersion
+ */
+deleteVersion : function () {},
+
+/**
+ * @method create
+ * @return A value converted from C/C++ "cocos2d::extension::AssetsManager*"
+ * @param {const char*}
+ * @param {const char*}
+ * @param {const char*}
+ * @param {std::function<void (int)>}
+ * @param {std::function<void (int)>}
+ * @param {std::function<void ()>}
+ */
+create : function () {},
+
+/**
+ * @method AssetsManager
+ * @constructor
+ */
+AssetsManager : function () {},
+
+};
+
+/**
  * @class CCBAnimationManager
  */
 cc.AnimationManager = {
