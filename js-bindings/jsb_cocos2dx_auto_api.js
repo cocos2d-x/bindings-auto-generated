f41186e7ef4209597d8c81952c3535b02305a79c
CocosRobot <redmine@cocos2d-x.org>
2014-01-13 23:07:38 +0000

    [AUTO] : autogenerated bindings

--- js-bindings/jsb_cocos2dx_auto_api.js
+++ js-bindings/jsb_cocos2dx_auto_api.js
@@ -1842,6 +1842,36 @@ create : function () {},
 cc.ActionCamera = {
 
 /**
+ * @method getEye
+ * @return A value converted from C/C++ "const kmVec3&"
+ */
+getEye : function () {},
+
+/**
+ * @method setUp
+ * @param {const kmVec3&}
+ */
+setUp : function () {},
+
+/**
+ * @method getCenter
+ * @return A value converted from C/C++ "const kmVec3&"
+ */
+getCenter : function () {},
+
+/**
+ * @method setCenter
+ * @param {const kmVec3&}
+ */
+setCenter : function () {},
+
+/**
+ * @method getUp
+ * @return A value converted from C/C++ "const kmVec3&"
+ */
+getUp : function () {},
+
+/**
  * @method ActionCamera
  * @constructor
  */
--- lua-bindings/lua_cocos2dx_auto.hpp
+++ lua-bindings/lua_cocos2dx_auto.hpp
@@ -1489,4 +1489,10 @@ int register_all_cocos2dx(lua_State* tolua_S);
 
 
 
+
+
+
+
+
+
 #endif // __cocos2dx_h__
